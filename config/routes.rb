28a8073ed71fc0cf38e64b22875a21becef3af8a
Trygve <trygvejohannessen88@gmail.com>
2022-02-11 04:23:50 +0100

    tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :tasks
   get 'say/hi'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "say#hi"
+  root "tasks#index"
 end
