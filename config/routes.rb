a1ff08c7729dbb3cd0fbcb39c3296a14d9534aa9
Trygve <trygvejohannessen88@gmail.com>
2022-02-19 04:06:57 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "posts#index"
+  root "user_tasks#index"
 end
